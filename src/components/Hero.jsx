391a7d806e1f5ac0465b6a116981aff0756f786b
Harsh Singh <harshsinghkumwar97819@gmail.com>
2025-11-08 11:32:06 +0530

    ffmped added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -88,7 +88,7 @@ const Hero = () => {
      </section>
 
      <div className="video absolute inset-0 ">
-        <video ref={videoRef} src="/videos/input.mp4" muted playsInline preload="auto"></video>
+        <video ref={videoRef} src="/videos/output.mp4" muted playsInline preload="auto"></video>
      </div>
   </> 
   );
