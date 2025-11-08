5a28a461b0875b7171f2a8f70264928ab03763cf
Harsh Singh <harshsinghkumwar97819@gmail.com>
2025-11-08 19:25:03 +0530

    menu section

---
+++ src/components/Menu.jsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useState } from "react";
+import { sliderLists } from "../../constants";
+
+const Menu = () => {
+  const [currentIndex, setCurrentIndex] = useState();
+  const totalCocktails = sliderLists.length
+  const goToSlide = (index) => {
+        const newIndex = (index + totalCocktails ) % totalCocktails
+        setCurrentIndex(newIndex)
+  } 
+
+  return (
+    <section id="menu" aria-labelledby="menu-heading">
+      <img
+        src="/images/slider-left-leaf.png"
+        alt="left-leaf"
+        id="m-left-leaf"
+      />
+      <img
+        src="/images/slider-right-leaf.png"
+        alt="right-leaf"
+        id="m-right-leaf"
+      />
+
+      <h2 id="menu-heading" className="sr-only">
+        Cocktail Menu
+      </h2>
+
+      <nav className="cocktail-tabs" aria-label="Cocktail Navigation">
+        {sliderLists.map((cocktail, index) => {
+          const isActive = index === currentIndex;
+
+          return (
+            <button
+              key={cocktail.id}
+              className={`${
+                isActive
+                  ? "text-white border-white"
+                  : "text-white/50 border-white/50"
+              }`} onClick={()=>goToSlide(index)}
+            >
+              {cocktail.name}
+            </button>
+          );
+        })}
+      </nav>
+
+      
+    </section>
+  );
+};
+
+export default Menu;
