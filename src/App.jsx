ca13d4033711e71337a40c3ff8fb942790ee8d6a
Harsh Singh <harshsinghkumwar97819@gmail.com>
2025-11-08 09:45:36 +0530

    leaf property added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import { ScrollTrigger , SplitText } from 'gsap/all'
 import gsap from 'gsap'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 gsap.registerPlugin(ScrollTrigger , SplitText)
 function App() {
   return (
    <main>
         <Navbar/>
+        <Hero/>
    </main>
   )
 }
